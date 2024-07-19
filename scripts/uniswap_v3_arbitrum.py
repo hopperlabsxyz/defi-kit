4e16c682b8a7bc6fc4d7a77287125cea9c883cf3
gdupont <gdupont.pro@gmail.com>
2024-07-19 10:52:27 +0200

    move univ3 logic to /lib/uniswap_v3.py and create a new script uniswap_v3_arbitrum.py to collect the data

---
+++ scripts/uniswap_v3_arbitrum.py
@@ -0,0 +1,3 @@
+from lib.uniswap_v3 import protocol_data
+
+protocol_data(blockchain="arbitrum", min_tvl_usd=1000000, min_volume_usd=1000000)
---
+++ sdk/src/protocols/uniswap/v3/_info_arbitrum.ts
@@ -0,0 +1,152 @@
+// This file is auto-generated. Do not edit!
+
+export default [
+  {
+    "address": "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    "symbol": "WETH"
+  },
+  {
+    "address": "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a",
+    "symbol": "GMX"
+  },
+  {
+    "address": "0x539bdE0d7Dbd336b79148AA742883198BBF60342",
+    "symbol": "MAGIC"
+  },
+  {
+    "address": "0x9842989969687f7d249d01Cae1D2ff6b7b6b6D35",
+    "symbol": "CRYPTO"
+  },
+  {
+    "address": "0x0c880f6761F1af8d9Aa9C466984b80DAb9a8c9e8",
+    "symbol": "PENDLE"
+  },
+  {
+    "address": "0x0341C0C0ec423328621788d4854119B97f44E391",
+    "symbol": "Silo"
+  },
+  {
+    "address": "0x912CE59144191C1204E64559FE8253a0e49E6548",
+    "symbol": "ARB"
+  },
+  {
+    "address": "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+    "symbol": "USDC"
+  },
+  {
+    "address": "0x6fD58f5a2F3468e35fEb098b5F59F04157002407",
+    "symbol": "pogai"
+  },
+  {
+    "address": "0x58b9cB810A68a7f3e1E4f8Cb45D1B9B3c79705E8",
+    "symbol": "NEXT"
+  },
+  {
+    "address": "0x6dAF586B7370B14163171544fca24AbcC0862ac5",
+    "symbol": "BPET"
+  },
+  {
+    "address": "0xB59c8912c83157a955f9D715E556257F432C35D7",
+    "symbol": "TRUF"
+  },
+  {
+    "address": "0x2C650dAb03A59332e2E0C0C4A7F726913e5028C1",
+    "symbol": "TAP"
+  },
+  {
+    "address": "0x9aee3C99934C88832399D6C6E08ad802112eBEab",
+    "symbol": "FU"
+  },
+  {
+    "address": "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
+    "symbol": "WBTC"
+  },
+  {
+    "address": "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",
+    "symbol": "UNI"
+  },
+  {
+    "address": "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+    "symbol": "USDT"
+  },
+  {
+    "address": "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4",
+    "symbol": "LINK"
+  },
+  {
+    "address": "0x18c11FD286C5EC11c3b683Caa813B77f5163A122",
+    "symbol": "GNS"
+  },
+  {
+    "address": "0x9623063377AD1B27544C965cCd7342f7EA7e88C7",
+    "symbol": "GRT"
+  },
+  {
+    "address": "0x3082CC23568eA640225c2467653dB90e9250AaA0",
+    "symbol": "RDNT"
+  },
+  {
+    "address": "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+    "symbol": "USDC"
+  },
+  {
+    "address": "0x83d6c8C06ac276465e4C92E7aC8C23740F435140",
+    "symbol": "HMX"
+  },
+  {
+    "address": "0x00CBcF7B3d37844e44b888Bc747bDd75FCf4E555",
+    "symbol": "XPET"
+  },
+  {
+    "address": "0x35751007a407ca6FEFfE80b3cB397736D2cf4dbe",
+    "symbol": "weETH"
+  },
+  {
+    "address": "0x092bAaDB7DEf4C3981454dD9c0A0D7FF07bCFc86",
+    "symbol": "MOR"
+  },
+  {
+    "address": "0x6985884C4392D348587B19cb9eAAf157F13271cd",
+    "symbol": "ZRO"
+  },
+  {
+    "address": "0xEC70Dcb4A1EFa46b8F2D97C310C9c4790ba5ffA8",
+    "symbol": "rETH"
+  },
+  {
+    "address": "0x431402e8b9dE9aa016C743880e04E517074D8cEC",
+    "symbol": "HEGIC"
+  },
+  {
+    "address": "0x5d3a1Ff2b6BAb83b63cd9AD0787074081a52ef34",
+    "symbol": "USDe"
+  },
+  {
+    "address": "0x18C14C2D707b2212e17d1579789Fc06010cfca23",
+    "symbol": "ETH+"
+  },
+  {
+    "address": "0x5979D7b546E38E414F7E9822514be443A4800529",
+    "symbol": "wstETH"
+  },
+  {
+    "address": "0xf1264873436A0771E440E2b28072FAfcC5EEBd01",
+    "symbol": "KNS"
+  },
+  {
+    "address": "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+    "symbol": "DAI"
+  },
+  {
+    "address": "0xD8369C2EDA18dD6518eABb1F85BD60606dEb39Ec",
+    "symbol": "WETH"
+  },
+  {
+    "address": "0x4186BFC76E2E237523CBC30FD220FE055156b41F",
+    "symbol": "rsETH"
+  },
+  {
+    "address": "0x2416092f143378750bb29b79eD961ab195CcEea5",
+    "symbol": "ezETH"
+  }
+] as const
