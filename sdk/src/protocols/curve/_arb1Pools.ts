8813d36f7a6c2a0c6260db5f968d67c00be9dc48
Julien FRANCAIS <jfrancai@student.42.fr>
2024-07-21 22:15:42 +0200

    updating curve ng stable pool addresses

--- sdk/src/protocols/curve/_arb1Pools.ts
+++ sdk/src/protocols/curve/_arb1Pools.ts
@@ -1,15 +1,379 @@
 export default [
   {
+    address: "0x73aF1150F265419Ef8a5DB41908B700C32D49135",
+    symbol: "crvUSD/USDT",
+    tokens: [
+      {
+        name: "crvUSD",
+        address: "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5",
+      },
+      {
+        name: "USDT",
+        address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+      },
+    ],
+  },
+  {
+    address: "0x3aDf984c937FA6846E5a24E0A68521Bdaf767cE1",
+    symbol: "crvUSD/USDC.e",
+    tokens: [
+      {
+        name: "crvUSD",
+        address: "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5",
+      },
+      {
+        name: "USDC.e",
+        address: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+      },
+    ],
+  },
+  {
     address: "0xec090cf6DD891D2d014beA6edAda6e05E025D93d",
-    symbol: "crvUSDC",
+    symbol: "crvUSD/USDC",
+    tokens: [
+      {
+        name: "crvUSD",
+        address: "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5",
+      },
+      {
+        name: "USDC",
+        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+      },
+    ],
+  },
+  {
+    address: "0xE920eEdAff6c3BEd1Ef61010B64d46986634E908",
+    symbol: "FRAXFXB20241231",
+    tokens: [
+      {
+        name: "FRAX",
+        address: "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F",
+      },
+      {
+        name: "FXB_20241231",
+        address: "0xC4C98e6feccfc0f5DD8CBEa9DCFedcFa8425eD97",
+      },
+    ],
+  },
+  {
+    address: "0x946adc524BD312D036776798c46ceDD948DD0A0f",
+    symbol: "FRAXFXB20261231",
+    tokens: [
+      {
+        name: "FRAX",
+        address: "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F",
+      },
+      {
+        name: "FXB_20261231",
+        address: "0xe3f7c2e873d8F73bCa7961E2D2e18Bbde2Bda7Ec",
+      },
+    ],
+  },
+  {
+    address: "0x2FE7AE43591E534C256A1594D326e5779E302Ff4",
+    symbol: "crvUSD/Frax",
+    tokens: [
+      {
+        name: "crvUSD",
+        address: "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5",
+      },
+      {
+        name: "FRAX",
+        address: "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F",
+      },
+    ],
+  },
+  {
+    address: "0x4070C044ABc8d9d22447DFE4B032405970878d06",
+    symbol: "MIM/crvUSD",
+    tokens: [
+      {
+        name: "crvUSD",
+        address: "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5",
+      },
+      {
+        name: "MIM",
+        address: "0xFEa7a6a0B346362BF88A9e4A88416B77a57D6c2A",
+      },
+    ],
+  },
+  {
+    address: "0x1446999B0b0E4f7aDA6Ee73f2Ae12a2cfdc5D9E7",
+    symbol: "Overnight max stable yield",
+    tokens: [
+      {
+        name: "USD+",
+        address: "0xe80772Eaf6e2E18B651F160Bc9158b2A5caFCA65",
+      },
+      {
+        name: "USDT+",
+        address: "0xb1084db8D3C05CEbd5FA9335dF95EE4b8a0edc30",
+      },
+    ],
+  },
+  {
+    address: "0x4bD135524897333bec344e50ddD85126554E58B4",
+    symbol: "USDC/USDM",
     tokens: [
       {
         name: "USDC",
         address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
       },
       {
+        name: "USDM",
+        address: "0x59D9356E565Ab3A36dD77763Fc0d87fEaf85508C",
+      },
+    ],
+  },
+  {
+    address: "0xeC9C5703B5CBc2d23574EEDE451E941304AC61a5",
+    symbol: "USDC/cUSDCv3",
+    tokens: [
+      {
+        name: "USDC",
+        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+      },
+      {
+        name: "cUSDCv3",
+        address: "0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
+      },
+    ],
+  },
+  {
+    address: "0x8958AE46De6b33293DDdc6cDfbe36900f4631851",
+    symbol: "zunUSD/crvUSD",
+    tokens: [
+      {
+        name: "zunUSD",
+        address: "0x870908873b6f940E025A7C6879678cB82eC6C9B6",
+      },
+      {
         name: "crvUSD",
         address: "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5",
+      },
+    ],
+  },
+  {
+    address: "0x7D22EA8877256437CC56075B856A714404Fe5343",
+    symbol: "USSD/USDT",
+    tokens: [
+      {
+        name: "USSD",
+        address: "0x33C88D4caC6aC34F77020915a2a88cd0417dC069",
+      },
+      {
+        name: "USDT",
+        address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+      },
+    ],
+  },
+  {
+    address: "0xe957cE03cCdd88f02ed8b05C9a3A28ABEf38514A",
+    symbol: "dlcBTC/WBTC",
+    tokens: [
+      {
+        name: "WBTC",
+        address: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
+      },
+      {
+        name: "DLCBTC",
+        address: "0x050C24dBf1eEc17babE5fc585F06116A259CC77A",
+      },
+    ],
+  },
+  {
+    address: "0xEBEEC2EDbbc66eB9055fe772b154f34d3dd686C8",
+    symbol: "zunETH/WETH",
+    tokens: [
+      {
+        name: "zunETH",
+        address: "0x06D65eC13465Ac5A4376dc101e1141252c4adDf8",
+      },
+      {
+        name: "WETH",
+        address: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+      },
+    ],
+  },
+  {
+    address: "0x1c34204FCFE5314Dcf53BE2671C02c35DB58B4e3",
+    symbol: "USDE/USDC",
+    tokens: [
+      {
+        name: "USDC",
+        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+      },
+      {
+        name: "USDe",
+        address: "0x5d3a1Ff2b6BAb83b63cd9AD0787074081a52ef34",
+      },
+    ],
+  },
+  {
+    address: "0x0aCacb4f6dB7708a5451B835acd39DFEbAC4EeB5",
+    symbol: "KNOX/rgUSD",
+    tokens: [
+      {
+        name: "KNOX",
+        address: "0x0BBF664D46becc28593368c97236FAa0fb397595",
+      },
+      {
+        name: "rgUSD",
+        address: "0x96a993f06951B01430523D0D5590192d650EBf3e",
+      },
+    ],
+  },
+  {
+    address: "0x95F425c7d730Eb7673fca272c2c305f48Ed546c3",
+    symbol: "ETH+/WETH",
+    tokens: [
+      {
+        name: "ETH+",
+        address: "0x18C14C2D707b2212e17d1579789Fc06010cfca23",
+      },
+      {
+        name: "WETH",
+        address: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+      },
+    ],
+  },
+  {
+    address: "0x67D11005AF05Bb1e9fDb1CFc261C23DE3E1055a1",
+    symbol: "eUSD/crvUSD",
+    tokens: [
+      {
+        name: "eUSD",
+        address: "0x12275DCB9048680c4Be40942eA4D92c74C63b844",
+      },
+      {
+        name: "crvUSD",
+        address: "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5",
+      },
+    ],
+  },
+  {
+    address: "0x93a416206B4ae3204cFE539edfeE6BC05a62963e",
+    symbol: "eUSD/USDC",
+    tokens: [
+      {
+        name: "eUSD",
+        address: "0x12275DCB9048680c4Be40942eA4D92c74C63b844",
+      },
+      {
+        name: "USDC",
+        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+      },
+    ],
+  },
+  {
+    address: "0x6f33daF91d2aCAe10F5CD7BbE3f31716Ed123F1D",
+    symbol: "KNOX/eUSD",
+    tokens: [
+      {
+        name: "KNOX",
+        address: "0x0BBF664D46becc28593368c97236FAa0fb397595",
+      },
+      {
+        name: "eUSD",
+        address: "0x12275DCB9048680c4Be40942eA4D92c74C63b844",
+      },
+    ],
+  },
+  {
+    address: "0x186cF879186986A20aADFb7eAD50e3C20cb26CeC",
+    symbol: "2BTC-ng",
+    tokens: [
+      {
+        name: "WBTC",
+        address: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
+      },
+      {
+        name: "tBTC",
+        address: "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
+      },
+    ],
+  },
+  {
+    address: "0x81fdbd700DB44C3e57639c2c4518630945c132A6",
+    symbol: "USDT/MONEY Curve LP",
+    tokens: [
+      {
+        name: "USDT",
+        address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+      },
+      {
+        name: "MONEY",
+        address: "0x69420f9E38a4e60a62224C489be4BF7a94402496",
+      },
+    ],
+  },
+  {
+    address: "0x73F8b9739a557d5822f0E431F5e03F614f3BD8A9",
+    symbol: "USDC/MONEY Curve LP",
+    tokens: [
+      {
+        name: "USDC",
+        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+      },
+      {
+        name: "MONEY",
+        address: "0x69420f9E38a4e60a62224C489be4BF7a94402496",
+      },
+    ],
+  },
+  {
+    address: "0xDF96c0334D628e2FD084111761aE1016f3A1Fb3D",
+    symbol: "DAI/MONEY Curve LP",
+    tokens: [
+      {
+        name: "DAI",
+        address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+      },
+      {
+        name: "MONEY",
+        address: "0x69420f9E38a4e60a62224C489be4BF7a94402496",
+      },
+    ],
+  },
+  {
+    address: "0xe699fD2B4EA36Af0CA07cEe042E0833aB831D067",
+    symbol: "FRAX/MONEY Curve LP",
+    tokens: [
+      {
+        name: "FRAX",
+        address: "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F",
+      },
+      {
+        name: "MONEY",
+        address: "0x69420f9E38a4e60a62224C489be4BF7a94402496",
+      },
+    ],
+  },
+  {
+    address: "0x8CE4E9d3246E3d629f8ed811c421054DC6542BD6",
+    symbol: "crvUSD/MONEY Curve LP",
+    tokens: [
+      {
+        name: "crvUSD",
+        address: "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5",
+      },
+      {
+        name: "MONEY",
+        address: "0x69420f9E38a4e60a62224C489be4BF7a94402496",
+      },
+    ],
+  },
+  {
+    address: "0x6E8E50f884Ac7e7814A8B0F7d5478Adcf11f50A4",
+    symbol: "sUSX/USDC",
+    tokens: [
+      {
+        name: "sUSX",
+        address: "0xbC404429558292eE2D769E57d57D6E74bbd2792d",
+      },
+      {
+        name: "USDC",
+        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
       },
     ],
   },
