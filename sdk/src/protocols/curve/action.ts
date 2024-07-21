4903dfc69a3db50098607615016deea75dc7b2f5
Julien FRANCAIS <jfrancai@student.42.fr>
2024-07-21 23:52:39 +0200

    curve action updated to handle withdraw from pool

--- sdk/src/protocols/curve/action.ts
+++ sdk/src/protocols/curve/action.ts
@@ -11,6 +11,24 @@ export const deposit = (pool: Pool, tokens: readonly Token[] = pool.tokens) => {
       ...allow.mainnet.curve.stableNGPool["add_liquidity(uint256[],uint256)"](),
       targetAddress: pool.address,
     },
+    {
+      ...allow.mainnet.curve.stableNGPool[
+        "remove_liquidity_one_coin(uint256,int128,uint256)"
+      ](),
+      targetAddress: pool.address,
+    },
+    {
+      ...allow.mainnet.curve.stableNGPool[
+        "remove_liquidity(uint256,uint256[])"
+      ](),
+      targetAddress: pool.address,
+    },
+    {
+      ...allow.mainnet.curve.stableNGPool[
+        "remove_liquidity_imbalance(uint256[],uint256)"
+      ](),
+      targetAddress: pool.address,
+    },
   ]
   if (tokenAddresses.length > 0) {
     permissions.push(...allowErc20Approve(tokenAddresses, [pool.address]))
