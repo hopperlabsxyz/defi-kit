dec06e02b7d586896ff664c2a98606f108789119
coviccinelle <coviccinelle@gmail.com>
2025-03-06 14:50:23 +0100

    spectra testing with deposit command 0x00

--- sdk/src/protocols/spectra/deposit.test.ts
+++ sdk/src/protocols/spectra/deposit.test.ts
@@ -6,8 +6,9 @@ import { parseEther, toBeHex } from "ethers"
 import { Chain, contracts } from "../../../src"
 import { getProvider } from "../../../test/provider"
 
-const weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-const input1 = "0x000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000001ba55f6289df860000000000000000000000007f39c581f595b53c5cb19bd0b3f8da6c935e2ca0"
+const weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+const input1 =
+  "0x000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000001ba55f6289df860000000000000000000000007f39c581f595b53c5cb19bd0b3f8da6c935e2ca0"
 // const inpu4 = ""
 describe("spectra", () => {
   describe("deposit action", () => {
@@ -17,13 +18,24 @@ describe("spectra", () => {
 
     it("deposit", async () => {
       const provider = getProvider(Chain.eth)
-      await provider.send("anvil_setBalance", [wallets.avatar, toBeHex(parseEther("10"))])
-      // contracts.mainnet.weth.a
+      const amount = toBeHex(parseEther("10"))
+      const router = contracts.mainnet.spectra.router
+      await provider.send("anvil_setBalance", [
+        wallets.avatar,
+        toBeHex(parseEther("10")),
+      ])
+      //trying the command 0x00 = uint256 constant TRANSFER_FROM = 0x00;
+      // /**
+      //  * Transfers tokens from msg.sender to the Router.
+      //   * (address token, uint256 value)
+      //   */
+
+      await kit.asMember.weth.approve(router, amount)
       await expect(
-        kit.asMember.spectra.router["execute(bytes,bytes[])"](
-          "0x00",
-          [input1]
-        )
+        kit.asMember.spectra.router["execute(bytes,bytes[])"]("0x00", [
+          weth,
+          amount,
+        ])
       ).not.toRevert()
     })
   })
