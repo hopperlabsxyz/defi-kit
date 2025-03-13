9164774df28fafc68001e7a06287f810ebb2a428
coviccinelle <coviccinelle@gmail.com>
2025-03-13 17:59:09 +0100

    WIP spectra tiny bit forward?

--- sdk/src/protocols/spectra/index.ts
+++ sdk/src/protocols/spectra/index.ts
@@ -3,22 +3,39 @@ import { allow } from "zodiac-roles-sdk/kit"
 // import { NotFoundError } from "../../errors"
 // import { Permission } from "zodiac-roles-sdk/."
 // import { allowErc20Approve } from "../../conditions"
-import { c } from "zodiac-roles-sdk"
+import { c, Permission } from "zodiac-roles-sdk"
 import { contracts } from "../../../eth-sdk/config"
+import { allowErc20Approve } from "../../conditions"
 
 const WSTETH = contracts.mainnet.lido.wstEth
+// Assert as `0x${string}`
+const xWSTETH: `0x${string}`[] = [WSTETH as `0x${string}`]
+const router = contracts.mainnet.spectra.router
+const xRouter: `0x${string}`[] = [router as `0x${string}`]
 
 export const eth = {
   deposit: async () => {
     const encoded = c.abiEncodedMatches(["0x00"], ["bytes1"])
     console.log("Expected: in the permission: ", encoded())
-    return [
-      allow.mainnet.spectra.router["execute(bytes,bytes[])"](
-        // c.abiEncodedMatches(["0x00"], ["bytes1"]), //command
-        // encoded,
-        undefined, //command
-        undefined //inputs
-      ),
-    ]
+    const permissions: Permission[] = []
+    permissions.push(
+      ...allowErc20Approve(xWSTETH, xRouter),
+      {
+        //token approve first
+        ...allow.mainnet.lido.wstEth.approve(
+          contracts.mainnet.spectra.router,
+          undefined
+        ),
+      },
+      {
+        ...allow.mainnet.spectra.router["execute(bytes,bytes[])"](
+          // c.abiEncodedMatches(["0x00"], ["bytes1"]), //command
+          // encoded,
+          undefined, //command
+          undefined //inputs
+        ),
+      }
+    )
+    return permissions
   },
 }
